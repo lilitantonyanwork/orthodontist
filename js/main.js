588589eb1b7b55ce08654026dd1ae84e007d8de5
lilitantonyan <lilit@helix.am>
2026-04-11 02:46:48 +0400

    add files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var swiper = new Swiper(".portfolio__list", {
     slidesPerView: 3,
-    spaceBetween: 20,
+    spaceBetween: 0,
     centeredSlides: true,
     loop: true,
+
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -11,4 +12,26 @@ var swiper = new Swiper(".portfolio__list", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        // When window width is >= 640px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+   const menu_btn = document.querySelector('.btn__menu');
+   const menu = document.querySelector('.menu');
+   const bb = document.querySelector('body');
+    menu_btn.addEventListener('click', function (){
+        menu.classList.toggle('show');
+        bb.classList.toggle('no-scroll')
+    })
 });
