ed5cf3431c3db4c967fd557f654c6bab175cd962
lilitantonyan <lilit@helix.am>
2026-04-10 16:41:40 +0400

    add files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".portfolio__list", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
